77628699830ba140335b288cdf63618218267bbd
Joao <silva.joao1@outlook.com.br>
2020-10-28 23:00:34 -0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <Text style={styles.title}>GitHub Timeline</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
@@ -18,4 +22,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 32,
+    color: '#000000',
+  }
 });
